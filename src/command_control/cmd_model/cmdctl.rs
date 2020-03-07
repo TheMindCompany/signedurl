e4797e946eb6b55b61180a3d07f5252e17baa76e
Brandon Clark <brandon.clark@gimbal.com>
2020-03-07 12:08:52 -0800

    Add region types to request structure

--- src/command_control/cmd_model/cmdctl.rs
+++ src/command_control/cmd_model/cmdctl.rs
@@ -58,8 +58,44 @@ impl CmdCtl {
     }
 
     pub fn region(&self) -> Region {
-        if self.region.is_empty() {
+        if self.region == "us-east-1" {
+            Region::UsEast1
+        } else if self.region == "us-east-2" {
             Region::UsEast2
+        } else if self.region == "ap-east-1" {
+            Region::ApEast1
+        } else if self.region == "ap-south-1" {
+            Region::ApSouth1
+        } else if self.region == "ap-northeast-1" {
+            Region::ApNortheast1
+        } else if self.region == "ap-northeast-2" {
+            Region::ApNortheast2
+        } else if self.region == "ap-northeast-3" {
+            Region::ApNortheast3
+        } else if self.region == "ap-southeast-1" {
+            Region::ApSoutheast1
+        } else if self.region == "ap-southeast-2" {
+            Region::ApSoutheast2
+        } else if self.region == "ca-central-1" {
+            Region::CaCentral1
+        } else if self.region == "cn-north-1" {
+            Region::CnNorth1
+        } else if self.region == "cn-northeast-1" {
+            Region::CnNorthwest1
+        } else if self.region == "eu-central-1" {
+            Region::EuCentral1
+        } else if self.region == "eu-north-1" {
+            Region::EuNorth1
+        } else if self.region == "eu-west-1" {
+            Region::EuWest1
+        } else if self.region == "eu-west-2" {
+            Region::EuWest2
+        } else if self.region == "eu-west-3" {
+            Region::EuWest3
+        } else if self.region == "me-south-1" {
+            Region::MeSouth1
+        } else if self.region == "sa-east-1" {
+            Region::SaEast1
         } else {
             Region::UsEast1
         }
